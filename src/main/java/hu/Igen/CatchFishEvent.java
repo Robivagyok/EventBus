09d7b18549880c3adb645e0d7330a58101b88c41
Robivagyok <krobi0516@gmail.com>
2022-02-07 18:38:21 +0100

    xddd

---
+++ src/main/java/hu/Igen/CatchFishEvent.java
@@ -0,0 +1,4 @@
+package hu.Igen;
+
+public class CatchFishEvent {
+}
--- src/main/java/hu/Igen/Fish.java
+++ src/main/java/hu/Igen/Fish.java
@@ -1,7 +1,20 @@
 package hu.Igen;
 
+import com.google.common.eventbus.EventBus;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Fish {
+    private EventBus eventbus;
+
+    @Autowired
+    public void setEventBus(EventBus eventbus){
+        this.eventbus=eventbus;
+    }
+
+    public void kifogtak(){
+        this.eventbus.post(new CatchFishEvent());
+    }
+
 }
